33ebb8302afe64db1752a58ac703b519078c672a
Chittaranjan Sardar <crsardar@gmail.com>
2020-02-13 00:02:06 +0530

    HTTPS Client test, using Apache httpclient

---
+++ spring-boot-rest-experiments/src/main/java/com/crsardar/handson/spring/boot/config/HttpClientConfig.java
@@ -0,0 +1,146 @@
+package com.crsardar.handson.spring.boot.config;
+
+/**
+ * @author Chittaranjan Sardar
+ */
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * - Supports both HTTP and HTTPS
+ * - Uses a connection pool to re-use connections and save overhead of creating connections.
+ * - Has a custom connection keep-alive strategy (to apply a default keep-alive if one isn't specified)
+ * - Starts an idle connection monitor to continuously clean up stale connections.
+ */
+@Configuration
+@EnableScheduling
+public class HttpClientConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientConfig.class);
+
+    // Determines the timeout in milliseconds until a connection is established.
+    private static final int CONNECT_TIMEOUT = 30000;
+
+    // The timeout when requesting a connection from the connection manager.
+    private static final int REQUEST_TIMEOUT = 30000;
+
+    // The timeout for waiting for data
+    private static final int SOCKET_TIMEOUT = 60000;
+
+    private static final int MAX_TOTAL_CONNECTIONS = 50;
+    private static final int DEFAULT_KEEP_ALIVE_TIME_MILLIS = 20 * 1000;
+    private static final int CLOSE_IDLE_CONNECTION_WAIT_TIME_SECS = 30;
+
+    @Bean
+    public PoolingHttpClientConnectionManager poolingConnectionManager() throws Exception{
+
+        SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
+        sslContextBuilder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+
+        SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build());
+
+        RegistryBuilder<ConnectionSocketFactory> registryBuilder = RegistryBuilder.<ConnectionSocketFactory>create();
+        registryBuilder.register("https", sslConnectionSocketFactory);
+        registryBuilder.register("http", new PlainConnectionSocketFactory());
+        Registry<ConnectionSocketFactory> registry = registryBuilder.build();
+
+        PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager(registry);
+        poolingConnectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
+
+        return poolingConnectionManager;
+    }
+
+    @Bean
+    public ConnectionKeepAliveStrategy connectionKeepAliveStrategy() {
+
+        return new ConnectionKeepAliveStrategy() {
+
+            @Override
+            public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+
+                HeaderIterator headerIterator = response.headerIterator(HTTP.CONN_KEEP_ALIVE);
+                HeaderElementIterator headerElementIterator = new BasicHeaderElementIterator(headerIterator);
+
+                while (headerElementIterator.hasNext()) {
+
+                    HeaderElement headerElement = headerElementIterator.nextElement();
+                    String param = headerElement.getName();
+                    String value = headerElement.getValue();
+
+                    if (value != null && param.equalsIgnoreCase("timeout")) {
+
+                        return Long.parseLong(value) * 1000;
+                    }
+                }
+                return DEFAULT_KEEP_ALIVE_TIME_MILLIS;
+            }
+        };
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient() throws Exception{
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(REQUEST_TIMEOUT)
+                .setConnectTimeout(CONNECT_TIMEOUT)
+                .setSocketTimeout(SOCKET_TIMEOUT).build();
+
+        return HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(poolingConnectionManager())
+                .setKeepAliveStrategy(connectionKeepAliveStrategy())
+                .build();
+    }
+
+    @Bean
+    public Runnable idleConnectionMonitor(final PoolingHttpClientConnectionManager connectionManager) {
+
+        return new Runnable() {
+            @Override
+            @Scheduled(fixedDelay = 10000)
+            public void run() {
+                try {
+                    if (connectionManager != null) {
+                        LOGGER.trace("run IdleConnectionMonitor - Closing expired and idle connections...");
+                        connectionManager.closeExpiredConnections();
+                        connectionManager.closeIdleConnections(CLOSE_IDLE_CONNECTION_WAIT_TIME_SECS, TimeUnit.SECONDS);
+                    } else {
+                        LOGGER.trace("run IdleConnectionMonitor - Http Client Connection manager is not initialised");
+                    }
+                } catch (Exception e) {
+                    LOGGER.error("run IdleConnectionMonitor - Exception occurred. msg={}, e={}", e.getMessage(), e);
+                }
+            }
+        };
+    }
+}
--- spring-boot-rest-experiments/src/main/java/com/crsardar/handson/spring/boot/controller/AppRestController.java
+++ spring-boot-rest-experiments/src/main/java/com/crsardar/handson/spring/boot/controller/AppRestController.java
@@ -1,10 +1,17 @@
 package com.crsardar.handson.spring.boot.controller;
 
 import com.crsardar.handson.spring.boot.com.crsardar.handson.spring.boot.model.Person;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @RestController
 public class AppRestController
@@ -27,9 +34,33 @@ public class AppRestController
         return person;
     }
 
+    @Autowired
+    CloseableHttpClient closeableHttpClient;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String test()
+    public String test() throws Exception
     {
+        HttpGet httpGet = new HttpGet("https://api.github.com/search/repositories?q=angular");
+
+        CloseableHttpResponse httpResponse = closeableHttpClient.execute(httpGet);
+
+        System.out.println("GET Response Status:: "
+                + httpResponse.getStatusLine().getStatusCode());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                httpResponse.getEntity().getContent()));
+
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = reader.readLine()) != null) {
+            response.append(inputLine);
+        }
+        reader.close();
+
+        // print result
+        System.out.println(response.toString());
+
         return "/spring-boot-rest-experiments : Is Up & Running!";
     }
 }
