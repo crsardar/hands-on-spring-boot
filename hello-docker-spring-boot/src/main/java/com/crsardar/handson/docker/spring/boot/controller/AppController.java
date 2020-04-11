a04718b99ec75403c1c95bb2b802bb0c85f72ccf
Chittaranjan Sardar <Chittaranjan.Sardar@Icumed.com>
2020-04-11 18:36:46 +0530

    hello-docker-spring-boot : Back up

--- hello-docker-spring-boot/src/main/java/com/crsardar/handson/docker/spring/boot/controller/AppController.java
+++ hello-docker-spring-boot/src/main/java/com/crsardar/handson/docker/spring/boot/controller/AppController.java
@@ -13,6 +13,6 @@ public class AppController
     @GetMapping("/")
     public String helloSpringBootInDocker(){
 
-        return "Hello World, SpringBoot indide a Docker Container!";
+        return "Hello World, SpringBoot inside a Docker Container!";
     }
 }
