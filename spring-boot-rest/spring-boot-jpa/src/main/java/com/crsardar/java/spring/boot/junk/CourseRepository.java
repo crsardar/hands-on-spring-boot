85b5633d2facb7598e7efbf53139cdd21059091f
Chittaranjan Sardar <Chittaranjan.Sardar@Icumed.com>
2020-10-02 16:59:22 +0530

    Hands On - JPA, with SpringBoot

--- spring-boot-rest/spring-boot-jpa/src/main/java/com/crsardar/java/spring/boot/junk/CourseRepository.java
+++ spring-boot-rest/spring-boot-jpa/src/main/java/com/crsardar/java/spring/boot/junk/CourseRepository.java
@@ -4,8 +4,10 @@ import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class CourseRepository
 {
     @Autowired
@@ -16,4 +18,23 @@ public class CourseRepository
         return em.find(Course.class, id);
     }
 
+    public void deleteById(final Long id)
+    {
+        Course course = findById(id);
+        em.remove(course);
+    }
+
+    public Course save(final Course course)
+    {
+        if (course.getId() == null)
+        {
+            em.persist(course);
+        }
+        else
+        {
+            em.merge(course);
+        }
+
+        return course;
+    }
 }
--- spring-boot-rest/spring-boot-jpa/src/test/java/com/crsardar/java/spring/boot/junk/CourseRepositoryTest.java
+++ spring-boot-rest/spring-boot-jpa/src/test/java/com/crsardar/java/spring/boot/junk/CourseRepositoryTest.java
@@ -8,8 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 
 @ExtendWith(SpringExtension.class) // Launch a SpringBoot Context. Old "@RunWith(SpringRunner.class)"
 @SpringBootTest(classes = JpaApplication.class) // - This make sures lunched SpringBoot Context is a Test Context
@@ -27,6 +27,42 @@ public class CourseRepositoryTest
         Course course = courseRepository.findById(11003L);
         Assertions.assertNotNull(course, "Course with Id = 11003 in not found.");
         Assertions.assertEquals("Spring Courses.", course.getName());
+    }
+
+    @Test
+    @DirtiesContext
+    public void testDeleteById()
+    {
+        Course course = courseRepository.findById(11003L);
+        Assertions.assertNotNull(course, "Course with Id = 11003 in not found.");
+        Assertions.assertEquals("Spring Courses.", course.getName());
+
+        courseRepository.deleteById(course.getId());
+        course = courseRepository.findById(11003L);
+        Assertions.assertNull(course);
+    }
+
+    @Test
+    @DirtiesContext
+    public void testSave()
+    {
+        // Save
+        Course course = new Course("Micro-Services playground.");
+        course = courseRepository.save(course);
+        Assertions.assertNotNull(course.getId());
+
+        // Update
+        course = courseRepository.findById(11003L);
+        Assertions.assertNotNull(course, "Course with Id = 11003 in not found.");
+        Assertions.assertEquals("Spring Courses.", course.getName());
+
+        course.setName("Cracking Kuberenet");
+        courseRepository.save(course);
+
+        course = courseRepository.findById(11003L);
+        Assertions.assertNotNull(course);
+        Assertions.assertEquals(11003L, course.getId());
+        Assertions.assertEquals("Cracking Kuberenet", course.getName());
     }
 
 }
